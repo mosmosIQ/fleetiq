3ca625aed60c072e296856c4abf5db6c93245223
mosmosIQ <alinacho49@gmail.com>
2026-05-12 03:30:15 +0400

    Prepare backend server for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,5 +2,5 @@ import app from "./app";
 import { env } from "./config/env";
 
 app.listen(env.PORT, () => {
-  console.log(`Backend running on port ${env.PORT}`);
+  console.log(`FleetIQ API running on port ${env.PORT}`);
 });
